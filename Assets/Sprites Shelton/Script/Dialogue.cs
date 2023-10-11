f35e23526ab6955431acbf2adfe8d0bcec69491c
Shelton Telag <sheltonstuarttelagvalderrama.97@gmail.com>
2023-10-11 16:12:31 -0500

    subiendo avamce del tuto

---
+++ Assets/Sprites Shelton/Script/Dialogue.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+
+    private bool isPlayerInRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            Debug.Log("entro a mensajes");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            Debug.Log("salio a mensajes");
+        }
+            
+    }
+}
