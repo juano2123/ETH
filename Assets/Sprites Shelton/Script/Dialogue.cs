26229994be2e85c8fa026863bc70f5517b7aedec
Shelton Telag <sheltonstuarttelagvalderrama.97@gmail.com>
2023-10-11 18:11:34 -0500

    terminando nivel de tutorial

--- Assets/Sprites Shelton/Script/Dialogue.cs
+++ Assets/Sprites Shelton/Script/Dialogue.cs
@@ -1,22 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
+    [SerializeField] private GameObject dialogueMark;
+    [SerializeField, TextArea(4,6)] private string[] dialogueLines;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+
+    [SerializeField] private GameObject npc;
 
     private bool isPlayerInRange;
-    // Start is called before the first frame update
+    private bool didDialogueStart;
+    private int lineIndex;
+    private float typingTime = 0.02f;
+
+    
+
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
-        
+        if (isPlayerInRange && Input.GetButtonDown("Fire1"))
+        {
+            if (!didDialogueStart)
+            {
+                starDialogue();
+            } else if(dialogueText.text == dialogueLines[lineIndex])
+            {
+                NextDialogueLine();
+            }
+        }
+    }
+
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+        if (lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            dialogueMark.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    private void starDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,6 +81,7 @@ public class Dialogue : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = true;
+            dialogueMark.SetActive(true);
             Debug.Log("entro a mensajes");
         }
     }
@@ -33,6 +91,7 @@ public class Dialogue : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = false;
+            dialogueMark.SetActive(false);
             Debug.Log("salio a mensajes");
         }
             
